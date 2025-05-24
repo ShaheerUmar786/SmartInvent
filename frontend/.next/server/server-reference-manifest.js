5563603a9a2b61347177a6d5cd32f8960edf6eb0
ShaheerUmar786 <shaheerdev112@gmail.com>
2025-05-24 13:56:28 -0700

    new next project uploaded

--- frontend/.next/server/server-reference-manifest.js
+++ frontend/.next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"process.env.NEXT_SERVER_ACTIONS_ENCRYPTION_KEY\"\n}"
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"2A04gsaVvUdGHbcgF7Clmmk2FiI24UXQavwu9Smtacs=\"\n}"
